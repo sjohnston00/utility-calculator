b3d465cb41ede668866042a958175a69676a7d09
Sam Johnston <johnstonsam712@gmail.com>
2023-03-17 12:29:18 +0000

    add manifest

--- app/root.tsx
+++ app/root.tsx
@@ -13,13 +13,36 @@ import tailwindStylesheetUrl from "./styles/tailwind.css";
 import { getUser } from "./session.server";
 
 export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
+  return [
+    { rel: "stylesheet", href: tailwindStylesheetUrl },
+    {
+      rel: "apple-touch-icon",
+      sizes: "152x152",
+      href: "/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon-16x16.png",
+    },
+    { rel: "manifest", href: "/site.webmanifest" },
+    { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#262626" },
+  ];
 };
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Utilities Calculator",
   viewport: "width=device-width,initial-scale=1",
+  "msapplication-TileColor": "#262626",
+  "theme-color": "#262626",
 });
 
 export async function loader({ request }: LoaderArgs) {
